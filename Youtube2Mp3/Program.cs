1998e20f8f2b20a5805b4f0954e7d0abb034fcf0
7kuik <7kuiker@gmail.com>
2025-06-17 21:05:43 -0700

    Adding support for naming your output file. Using FFmpeg to correctly convert to an MP3 file

--- Youtube2Mp3/Program.cs
+++ Youtube2Mp3/Program.cs
@@ -7,18 +7,28 @@ Console.WriteLine("Initializing Youtube 2 MP3 Converter!!");
 
 string ytDlpPath = @"C:\ProgramData\chocolatey\lib\yt-dlp\tools\x64\yt-dlp.exe";
 string youtubeUrl = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+string ffmpegDir = @"C:\ProgramData\chocolatey\lib\ffmpeg\tools\ffmpeg\bin";
 string outputDir = @"C:\Music\MyMP3s";
+string outputFileName;
+
+Console.WriteLine("Enter the  Youtube link you wish to convert to MP3: ");
+
+youtubeUrl = Console.ReadLine();
+
+Console.WriteLine("Enter the name of the MP3 that will be generated: ");
+
+outputFileName = Console.ReadLine();
 
 var process = new Process
 {
     StartInfo = new ProcessStartInfo
     {
         FileName = ytDlpPath,
-        Arguments = $"-x --audio-format mp3 -o \"{outputDir}\\%(title)s.%(ext)s\" \"{youtubeUrl}\"",
+        Arguments = $@"--ffmpeg-location ""{ffmpegDir}"" -x --audio-format mp3 -o ""{outputDir}\\{outputFileName}.%(ext)s"" ""{youtubeUrl}""",
         RedirectStandardOutput = true,
         RedirectStandardError = true,
         UseShellExecute = false,
-        CreateNoWindow = true
+        CreateNoWindow = true,
     }
 };
 
