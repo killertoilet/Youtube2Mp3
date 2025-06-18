0813d9df2276315230b6f14908be2e12f18ffff5
7kuik <7kuik@GIRTHQUAKE>
2025-06-17 20:23:58 -0700

    Add project files.

---
+++ Youtube2Mp3/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+
+Console.WriteLine("Initializing Youtube 2 MP3 Converter!!");
+
+
+string ytDlpPath = @"C:\ProgramData\chocolatey\lib\yt-dlp\tools\x64\yt-dlp.exe";
+string youtubeUrl = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+string outputDir = @"C:\Music\MyMP3s";
+
+var process = new Process
+{
+    StartInfo = new ProcessStartInfo
+    {
+        FileName = ytDlpPath,
+        Arguments = $"-x --audio-format mp3 -o \"{outputDir}\\%(title)s.%(ext)s\" \"{youtubeUrl}\"",
+        RedirectStandardOutput = true,
+        RedirectStandardError = true,
+        UseShellExecute = false,
+        CreateNoWindow = true
+    }
+};
+
+process.Start();
+string output = process.StandardError.ReadToEnd(); // yt-dlp logs here
+process.WaitForExit();
+Console.WriteLine("Download and conversion complete!");
